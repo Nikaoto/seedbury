dbb23840a906652459e323d908de887fc19b88dd
Nikaoto <nikaoto@gmail.com>
2018-07-30 22:04:26 +0400

    move some statics into class

--- Classes/Plant.h
+++ Classes/Plant.h
@@ -2,18 +2,20 @@
 #include "cocos2d.h"
 
 const static cocos2d::Size PLANT_SIZE = cocos2d::Size(128, 128);
-const static int MIN_GROWTH_STAGE = 0;
-const static int MAX_GROWTH_STAGE = 3;
 const static std::vector<std::string> TEXTURE_PATHS = {
     "plant_0.png",
     "plant_1.png",
     "plant_2.png",
     "plant_3.png"
 };
-const static int GROWTH_TIME = 5;
 
 class Plant : public cocos2d::Sprite {
 public:
+    static const int GROWTH_TIME = 5;
+    static const int MIN_GROWTH_STAGE = 0;
+    static const int MAX_GROWTH_STAGE = 3;
+    static const int MARGIN_BOTTOM = 55;
+    
     bool init() override;
     void update(float dt) override;
     
